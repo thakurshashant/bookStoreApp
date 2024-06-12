b7b2d3fed19feeb0e19362991a3c9617b16fca5f
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-12 19:16:57 +0530

    front end integrated

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bookRoute from './route/book.route.js';
-
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
