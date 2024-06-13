aca9a1c1ca4728554a4d6d964c251a4caa9d0e27
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-13 18:05:43 +0530

    login controller added

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -26,3 +26,25 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const login =async (req,res)=>{
+    try {
+        const {email, password} =req.body;
+        const user = await User.findOne({email});
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if(!user || !isMatch){
+            return res.status(400).json({message:"Invalid username or password"});
+        }
+        else{
+            res.status(200).json({message:"Login successful" , user: {
+                fullname: user.fullname, 
+                email: user.email,
+                _id:user._id,
+            }
+            })
+        }
+    } catch (error) {
+        console.log("Error: " + error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
