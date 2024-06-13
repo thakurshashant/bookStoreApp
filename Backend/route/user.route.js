f6225cb13732da083c493670d08b4b90a354ef0b
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-13 18:43:13 +0530

    login completed

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,10 +1,11 @@
 //defining routes for the user model
 import express from 'express';
 import { signup } from '../controller/user.controller.js';
+import { login } from '../controller/user.controller.js';
 const router=express.Router();
 
 
 router.post("/signup" , signup); //calling the signup function from user.controller.js
+router.post("/login" , login); //calling the login function from user.controller.js
 
-
-export default router;
+export default router; 
