371ddc6cadc4ab008815253f90b96bac8814d895
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-13 18:55:54 +0530

    sign up added to backend

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Login from './Login'
 import { useForm } from "react-hook-form"
+import axios from 'axios'
 
 function Signup() {
     const {
@@ -10,7 +11,23 @@ function Signup() {
         formState: { errors },
       } = useForm()
     
-      const onSubmit = (data) => console.log(data)
+      const onSubmit = async (data) => {
+        const userInfo={
+          fullname:data.fullname,
+          email:data.email,
+          password:data.password
+        }
+        await axios.post("http://localhost:4001/user/signup", userInfo)
+        .then((res) =>{
+          console.log(res.data)
+          if(res.data){
+            alert("Sign up Successfull");
+          }
+        }).catch((err) =>{
+          console.log( err);
+          alert("Error: "+err);
+        })
+      }
   return (
     <> 
 <div className=" flex h-screen items-center justify-center">
@@ -26,9 +43,9 @@ function Signup() {
       <span>Name</span>
       <br />
       <input type="text"
-      placeholder="Enter your fullname"  className="w-80 px-3 py-1 border rounded-md outline-none" {...register("name", { required: true })}/>
+      placeholder="Enter your fullname"  className="w-80 px-3 py-1 border rounded-md outline-none" {...register("fullname", { required: true })}/>
       <br />
-      {errors.name && <span className=" text-sm text-red-500">This field is required</span>}
+      {errors.fullname && <span className=" text-sm text-red-500">This field is required</span>}
       
     </div>
     {/* emial */}
