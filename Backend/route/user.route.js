48e01cda39be706da8602f4832bbb166d5577c0a
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-13 17:34:26 +0530

    Routes added for user

---
+++ Backend/route/user.route.js
@@ -0,0 +1,10 @@
+//defining routes for the user model
+import express from 'express';
+import { signup } from '../controller/user.controller';
+const router=express.Router();
+
+
+router.post("/signup" , signup); //calling the signup function from user.controller.js
+
+
+export default router;
